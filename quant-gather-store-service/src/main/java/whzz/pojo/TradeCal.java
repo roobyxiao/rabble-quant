f99f7af8ca08892efd738b14bcb999ffd79a2726
roobyxiao <roobyxiao@gmail.com>
2021-11-06 04:39:13 +0800

    ff

--- quant-gather-store-service/src/main/java/whzz/pojo/TradeCal.java
+++ quant-gather-store-service/src/main/java/whzz/pojo/TradeCal.java
@@ -1,9 +1,10 @@
 package whzz.pojo;
 
 import com.alibaba.fastjson.annotation.JSONField;
+import lombok.Data;
 
 import java.sql.Date;
-
+@Data
 public class TradeCal {
     @JSONField(name = "calendar_date")
     private Date date;
